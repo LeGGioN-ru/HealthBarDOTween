a9e62e36cf77854b6d2b1a43784a284adb126ec7
LeGGioN-ru <nikita90017@gmail.com>
2022-06-13 20:17:20 +0300

    division of responsibility
    Health bar and Health bar view

--- Assets/Script/HealthBar.cs
+++ Assets/Script/HealthBar.cs
@@ -2,47 +2,32 @@ using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
-[RequireComponent(typeof(Slider))]
 public class HealthBar : MonoBehaviour
 {
-    [SerializeField] private float _changeDelay;
+    [SerializeField] private HealthBarView _healthBarView;
 
-    private Slider _healthBar;
     private float _addRemoveHealth = 10;
     private float _targetHealth;
-    private Tweener _tweener;
 
     public void AddHealth()
     {
-        if (_targetHealth + _addRemoveHealth <= _healthBar.maxValue)
+        int maxHealth = 100;
+
+        if (_targetHealth + _addRemoveHealth <= maxHealth)
         {
             _targetHealth += _addRemoveHealth;
-            _tweener.ChangeEndValue(_targetHealth, true).Restart();
+            _healthBarView.ChangeHealth(_targetHealth);
         }
     }
 
     public void RemoveHealth()
     {
-        if (_targetHealth - _addRemoveHealth >= _healthBar.minValue)
+        int minHealth = 0;
+
+        if (_targetHealth - _addRemoveHealth >= minHealth)
         {
             _targetHealth -= _addRemoveHealth;
-            _tweener.ChangeEndValue(_targetHealth, true).Restart();
-        }
-    }
-
-    private void Start()
-    {
-        _healthBar = GetComponent<Slider>();
-        _tweener = _healthBar.DOValue(_targetHealth, _changeDelay).SetAutoKill(false);
-    }
-
-    private void OnValidate()
-    {
-        float minDelay = 0.1f;
-
-        if (_changeDelay <= 0)
-        {
-            _changeDelay = minDelay;
+            _healthBarView.ChangeHealth(_targetHealth);
         }
     }
 }
